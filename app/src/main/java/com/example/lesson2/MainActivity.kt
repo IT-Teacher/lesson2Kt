931a8b8ee2c8be94c47bb05f65f03622bf4cd7a8
Admin <mirgiyozom@gmail.com>
2022-03-17 22:00:56 +0500

    add SecondActivity

--- app/src/main/java/com/example/lesson2/MainActivity.kt
+++ app/src/main/java/com/example/lesson2/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.lesson2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 
@@ -9,6 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button=findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE,"")
+            }
+            startActivity(intent)
+        }
+
 
 
     }
