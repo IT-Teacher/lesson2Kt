f8a419f9be47c350ba52cc92b5226f5f380f49c3
Admin <mirgiyozom@gmail.com>
2022-03-17 21:30:37 +0500

    text change

--- app/src/main/java/com/example/lesson2/MainActivity.kt
+++ app/src/main/java/com/example/lesson2/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.lesson2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
     }
 }
